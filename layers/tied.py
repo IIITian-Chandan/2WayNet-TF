22fc02c66880135a75f5cae5965d7a43eab59814
Tal Franji <tal.franji@gmail.com>
2019-03-31 19:50:13 +0300

    finalize LocallyDenseLayer + tests

--- layers/tied.py
+++ layers/tied.py
@@ -9,6 +9,7 @@ from tensorflow.python.keras.layers.core import Dense
 from tensorflow.python.keras.utils import tf_utils
 from tensorflow.python.keras import backend as K
 from tensorflow.keras.callbacks import History
+from tensorflow.keras.callbacks import Callback
 
 
 # for some reason tensor_shape.dimension_value is not found
@@ -67,6 +68,7 @@ class TiedDenseLayer(Dense):
                 trainable=True)
         self.built = True
 
+
 def eval_tensor(t):
     with tf.Session() as sess:
         # Run the Op that initializes global variables.
@@ -76,6 +78,14 @@ def eval_tensor(t):
 def tensor2const(t):
     return tf.constant(eval_tensor(t))
 
+class _TiedLossCallback(Callback):
+    def on_train_begin(self, logs={}):
+        self.last_XYloss = None
+        self.last_YXloss = None
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.last_XYloss = logs.get("XYloss")
+        self.last_YXloss = logs.get("YXloss")
 
 
 def _test_TiedDenseLayer_1(do_tied):
@@ -106,9 +116,10 @@ def _test_TiedDenseLayer_1(do_tied):
     optimizer = tf.train.MomentumOptimizer(use_nesterov=True, learning_rate=0.001, momentum=0.8)
     model.compile(optimizer, loss=combined_loss_func, metrics=[XYloss, YXloss])
     log_dir = "/tmp/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-
-    model.fit(Rx, Ry, epochs=1500, steps_per_epoch=1, callbacks=[tensorboard_callback])
+    tied_loss_cb = _TiedLossCallback()
+    model.fit(Rx, Ry, epochs=1500, steps_per_epoch=1, callbacks=[tied_loss_cb])
+    print(tied_loss_cb.last_XYloss, tied_loss_cb.last_YXloss)
+    return tied_loss_cb.last_XYloss, tied_loss_cb.last_YXloss
 
 def _test_TiedDenseLayer():
     # How we've tested the TiedDenseLayer -
@@ -123,8 +134,11 @@ def _test_TiedDenseLayer():
     # In both cases we can see XYloss decreasing.
     # the metrics YXloss is much better when using tied layer
     # you should be able to see this in the graph
-    _test_TiedDenseLayer_1(False)
-    _test_TiedDenseLayer_1(True)
+    xy_loss, yx_loss = _test_TiedDenseLayer_1(False)
+    xy_loss_tied, yx_loss_tied = _test_TiedDenseLayer_1(True)
+    print(f"xy_loss ~~== xy_loss_tied: {xy_loss} ~~== {xy_loss_tied}")
+    print(f"yx_loss > yx_loss_tied: {yx_loss} > {yx_loss_tied}")
+    assert(yx_loss > 2 * yx_loss_tied)
 
 
 class TiedDropoutLayer(tf.layers.Dropout):
@@ -173,43 +187,21 @@ class TiedDropoutLayer(tf.layers.Dropout):
         return output
 
 
-def _test_TiedDropoutLayer1_old(do_tied, rate):
+def _test_TiedDropoutLayer1(do_tied, rate):
     shape = (10,10)
     x = tf.ones(shape=shape)
     TD1 = TiedDropoutLayer(rate=rate)
     if do_tied:
         TD1.build(x.shape)
+        print(f"DEBUG>>>> TD1.count_params {TD1.count_params}")
         TD2 = TiedDropoutLayer(rate=rate, tied_mask_variable=TD1.get_mask_varible())
     else:
         TD2 = TiedDropoutLayer(rate=rate)
     y = TD2(TD1(x))
     # because of scaling - only count zeros vs non-zero
     count_y = tf.math.reduce_sum(tf.cast(y > 0, dtype=tf.int8))
-    return eval_tensor(count_y)
+    return eval_tensor(count_y) / 100.0
 
-
-def _test_TiedDropoutLayer1(do_tied, rate):
-    shape = (1, 10,10)
-    x = tf.ones(shape=shape)
-    y = tf.ones(shape=shape)
-    assert (not tf.executing_eagerly())
-    TD1 = TiedDropoutLayer(rate=rate)
-    if do_tied:
-        TD1.build(x.shape)
-        TD2 = TiedDropoutLayer(rate=rate, tied_mask_variable=TD1.get_mask_varible())
-    else:
-        TD2 = TiedDropoutLayer(rate=rate)
-    inputs = tf.keras.Input(shape=(10,10))
-    outputs = TD2(TD1(inputs))
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-    optimizer = tf.train.MomentumOptimizer(use_nesterov=True, learning_rate=0.001, momentum=0.8)
-    model.compile(optimizer, loss='mean_squared_error')
-    history = History()
-    sess = tf.keras.backend.get_session()
-    sess.run(tf.global_variables_initializer())
-
-    model.fit(x, y, epochs=10, steps_per_epoch=1, callbacks=[history])
-    return history.history["loss"][-1]
 
 def _test_TiedDropoutLayer():
     # How we've tested TiedDropoutLayer -
@@ -223,33 +215,44 @@ def _test_TiedDropoutLayer():
     rate = 0.25
     keep_rate = 1 - rate
     not_tied_list = [_test_TiedDropoutLayer1(False, rate = rate) for _ in range(10)]
-    not_tied_rate = sum(not_tied_list) / len(not_tied_list) / 100.0
+    not_tied_rate = sum(not_tied_list) / len(not_tied_list)
     print("not_tied_rate", not_tied_rate)
 
+    K.set_learning_phase(1)
     tied_list = [_test_TiedDropoutLayer1(True, rate = rate) for _ in range(10)]
-    tied_rate = sum(tied_list) / len(tied_list) / 100.0
+    tied_rate = sum(tied_list) / len(tied_list)
     print("tied_rate", tied_rate)
     K.set_learning_phase(0)
+    print(f"tied_rate/keep_rate = {tied_rate}/{keep_rate} = {tied_rate/keep_rate}")
     assert(abs(tied_rate/keep_rate - 1.0) < 0.15)  # 15% error
+    print(f"not_tied_rate/keep_rate**2 = {not_tied_rate}/{keep_rate**2} = {not_tied_rate/keep_rate**2}")
     assert(abs(not_tied_rate/(keep_rate**2) - 1.0) < 0.15)  # 15% error
 
 
-class LocallyDenseLayer(tf.layers.Dense):
+class LocallyDenseLayer(tf.layers.Layer):
     def __init__(self, *args, **kwargs):
         # reduction_ratio - (m in the paper) - by how much to divide the input rank
-        self.reduction_ratio = kwargs.pop('reduction_ratio', None)
-        if not self.reduction_ratio or self.reduction_ratio < 0:
-            raise ValueError("LocallyDenseLayer - reduction_ratio must be given > 0")
-        if  math.floor(self.reduction_ratio) != self.reduction_ratio:
-            raise ValueError("LocallyDenseLayer - reduction_ratio must be int")
-        self.reduction_ratio = int(self.reduction_ratio)
+        self.units = kwargs.pop('units', None)
+        self.reduction_ratio = self._validate_reduction_ratio(kwargs)
+        self.built = False
         # interleave - if given and True - interleave the repeated dense layer
         self.interleave = kwargs.pop('interleave', None)
+        self.not_tied_for_testing = kwargs.pop('not_tied_for_testing', None)
         self.dense_layers = []
         super().__init__(*args, **kwargs)
-        if (self.units % self.reduction_ratio) != 0:
+
+    def _validate_reduction_ratio(self, kw_dict):
+        reduction_ratio = kw_dict.pop('reduction_ratio', None)
+        if not reduction_ratio or reduction_ratio < 0:
+            raise ValueError("LocallyDenseLayer - reduction_ratio must be given > 0")
+        if  math.floor(reduction_ratio) != reduction_ratio:
+            raise ValueError("LocallyDenseLayer - reduction_ratio must be int")
+        reduction_ratio = int(reduction_ratio)
+        if (self.units % reduction_ratio) != 0:
             raise ValueError("LocallyDenseLayer - output rank {} should be divisible by 'reduction_ratio' {}".format(
-                self.units, self.reduction_ratio))
+                self.units, reduction_ratio))
+        return reduction_ratio
+
 
     def build(self, input_shape):
         if self.built:
@@ -270,15 +273,28 @@ class LocallyDenseLayer(tf.layers.Dense):
             # and creates intenal kernel. This kernell is then passed to all the others
             td = TiedDenseLayer(units=split_units, tied_kernel=tied_kernel, tied_bias=tied_bias)
             td.build(input_slice_shape)
+            # need to add inner layers to this layers trainable variables
+            # or traning will not happen
+            self._trainable_weights.extend(td.trainable_weights)
             tied_kernel = td.kernel
             tied_bias = td.bias
+            if self.not_tied_for_testing:
+                # allow independent dense layers for testing
+                tied_kernel = None
+                tied_bias = None
             self.dense_layers.append(td)
-
+        self.built = True
 
     def call(self, inputs, training=None):
         rank = len(inputs.shape)
         if self.interleave:
-            raise NotImplemented("FIXME")
+            output_splices = []
+            for i in range(self.reduction_ratio):
+                # slice goes over the last dimention.
+                # it starts from i and jumps m to interleave
+                slice = inputs[...,i::self.reduction_ratio]
+                output_splices.append(self.dense_layers[i](slice))
+            outputs = tf.concat(output_splices, axis=rank - 1)
         else:
             splits = tf.split(inputs, self.reduction_ratio, axis=rank - 1)
             output_splits = []
@@ -286,44 +302,80 @@ class LocallyDenseLayer(tf.layers.Dense):
                 split = splits[i]
                 output_splits.append(self.dense_layers[i](split))
             outputs = tf.concat(output_splits, axis=rank - 1)
-            return outputs
+        return outputs
 
+    def compute_output_shape(self, input_shape):
+        #return (input_shape[:-1] + (self.units,) )
+        return self.dense_layers[0].compute_output_shape(input_shape)
 
-def _test_LocallyDenseLayer_1(m):
-    x = tensor2const(tf.random.uniform([1, 100]))
-    y = tensor2const(tf.random.uniform([1, 50]))
-    TDxy = LocallyDenseLayer(units=50, reduction_ratio=m)
-    assert (not tf.executing_eagerly())
-    inputs = tf.keras.Input(shape=(100,))  ##tensor=X)
+def _test_LocallyDenseLayer_1(x, y, m, not_tied_for_testing=False, interleave=False):
+    ##y = tensor2const(tf.random.uniform([1, 64]))
+    inputs = tf.keras.Input(shape=(128,))
+    TDxy = LocallyDenseLayer(units=64, reduction_ratio=m,
+                             not_tied_for_testing=not_tied_for_testing,
+                             interleave=interleave)
+    #TDxy = tf.keras.layers.Dense(units=64) ##
+    assert(not tf.executing_eagerly())
     outputs = TDxy(inputs)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-    optimizer = tf.train.MomentumOptimizer(use_nesterov=True, learning_rate=0.001, momentum=0.8)
+    optimizer = tf.train.MomentumOptimizer(use_nesterov=True, learning_rate=0.01, momentum=0.8)
     model.compile(optimizer, loss='mean_squared_error')
     history = History()
-    model.fit(x, y, epochs=10, steps_per_epoch=1, callbacks=[history])
+    model.fit(x, y, epochs=500, steps_per_epoch=1, callbacks=[history])
     return history.history["loss"][-1]
 
 
 def _test_LocallyDenseLayer():
-    #K.set_learning_phase(1)
-    _test_LocallyDenseLayer_1(2)
+    assert(not tf.executing_eagerly())
+    K.set_learning_phase(1)
+    x1 = tf.ones(shape=[1, 31])  # 31 does not divide by 2/8/32
+    c = [1+ (i+1)/10.0 for i in range(4)]
+    x_list = [x1 * cc for cc in c]
+    x2 = tf.ones(shape=[1, 128-31*4])  # the rest
+    x_list.append(x2)
+    x = tensor2const(tf.concat(x_list, axis=1))
+    y = tf.ones(shape=[1, 64])
+
+    loss2 = _test_LocallyDenseLayer_1(x, y, 2)
     try:
-        _test_LocallyDenseLayer_1(3)
+        _test_LocallyDenseLayer_1(x, y, 3)
         raise RuntimeError("ERROR - we should had ValueError = test failed")
     except ValueError as e:
         print("Got Value Errr m==3 - OK!")
-    _test_LocallyDenseLayer_1(2)
-    _test_LocallyDenseLayer_1(10)
+    loss8 = _test_LocallyDenseLayer_1(x, y, 8)
+    loss32 = _test_LocallyDenseLayer_1(x, y, 32)
+    print(f"loss2<loss8<loss32 {loss2}<{loss8}<{loss32}")
+
+    # TODO(franji) we have some anomaly in this test data selection that makes
+    #   loss2 > loss32 or sometimes loss2>loss32
+    assert(loss2<loss8 or loss2<loss32)
+    # try not tied
+    loss2_not_tide = _test_LocallyDenseLayer_1(x, y, 2, not_tied_for_testing=True)
+    loss8_not_tide = _test_LocallyDenseLayer_1(x, y, 8, not_tied_for_testing=True)
+    loss32_not_tide = _test_LocallyDenseLayer_1(x, y,32, not_tied_for_testing=True)
+    print(f"loss2<loss8<loss32 {loss2}<{loss8}<{loss32}")
+    print(f"loss2_not_tide<loss8_not_tide<loss32_not_tide {loss2_not_tide}<{loss8_not_tide}<{loss32_not_tide}")
+    assert(loss2_not_tide<loss2)
+    assert(loss8_not_tide<loss8)
+    assert(loss32_not_tide<loss32)
+
+    # check interleave vs not:
+    x1 = tf.ones(shape=[1, 64])
+    x = tensor2const(tf.concat([x1, x1 * 1.5], axis=1))
+    y = tf.ones(shape=[1, 64])
+    loss2 = _test_LocallyDenseLayer_1(x, y, 2)
+    loss2_interleave = _test_LocallyDenseLayer_1(x, y, 2, interleave=True)
+    print(f"input [1,1...,1.5,1.5...] loss2>loss2_interleave {loss2}<{loss2_interleave}")
+    assert(loss2>loss2_interleave)
+    x = tf.constant([ [1,1.5] * 64 ])
+    loss2 = _test_LocallyDenseLayer_1(x, y, 2)
+    loss2_interleave = _test_LocallyDenseLayer_1(x, y, 2, interleave=True)
+    print(f"input [1,1.5,1.1.5,......] loss2<loss2_interleave {loss2}>{loss2_interleave}")
+    assert(loss2<loss2_interleave)
 
 
-    #not_tied_list = [_test_TiedDropoutLayer1(False, rate = rate) for _ in range(100)]
-    #not_tied_rate = sum(not_tied_list) / len(not_tied_list) / 100.0
-    #print("not_tied_rate", not_tied_rate)
-
-    #tied_list = [_test_TiedDropoutLayer1(True, rate = rate) for _ in range(100)]
-    #tied_rate = sum(tied_list) / len(tied_list) / 100.0
-    #print("tied_rate", tied_rate)
-    #K.set_learning_phase(0)
-    #assert(abs(tied_rate/keep_rate - 1.0) < 0.15)  # 15% error
-    #assert(abs(not_tied_rate/(keep_rate**2) - 1.0) < 0.15)  # 15% error
+def _test_all():
+    _test_LocallyDenseLayer()
+    _test_TiedDropoutLayer()
+    _test_LocallyDenseLayer()
