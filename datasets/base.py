3df031dd7530fa555e70227f5c25e11333200f9d
Tal Franji <tal.franji@gmail.com>
2019-04-18 16:42:08 +0300

    experimets with colab

--- datasets/base.py
+++ datasets/base.py
@@ -27,20 +27,21 @@ class BaseDataset(object):
 
 class MNISTDataset(BaseDataset):
     def load(self):
-        mnist = tf.keras.datasets.mnist
-        (z_train, label_train),(z_test, label_test) = mnist.load_data()
-        # are the full images - out x and y are the two halves of the images
-        img_sz = z_train.shape[1] * z_train.shape[2]
-        def flatten(z):
-            return tf.reshape(z / 255.0, shape=(z.shape[0], img_sz))
-        flat_train = flatten(z_train)
-        flat_test = flatten(z_test)
-        # upper half of image
-        self._x_train = flat_train[:,:img_sz // 2]
-        self._x_test = flat_test[:,:img_sz // 2]
-        # lower half of image
-        self._y_train = flat_train[:,img_sz // 2:]
-        self._y_test = flat_test[:,img_sz // 2:]
+        with tf.Session() as sess:
+            mnist = tf.keras.datasets.mnist
+            (z_train, label_train),(z_test, label_test) = mnist.load_data()
+            # are the full images - out x and y are the two halves of the images
+            img_sz = z_train.shape[1] * z_train.shape[2]
+            def flatten(z):
+                return tf.reshape(z / 255.0, shape=(z.shape[0], img_sz))
+            flat_train = flatten(z_train)
+            flat_test = flatten(z_test)
+            # upper half of image
+            self._x_train = sess.run(flat_train[:,:img_sz // 2])
+            self._x_test = sess.run(flat_test[:,:img_sz // 2])
+            # lower half of image
+            self._y_train = sess.run(flat_train[:,img_sz // 2:])
+            self._y_test = sess.run(flat_test[:,img_sz // 2:])
 
     def x_train(self):
         return self._x_train
