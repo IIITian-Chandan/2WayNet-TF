8d1ca5e315cfd2433fbe7d6328cd19c897730750
chandan mishra <chandan.mishra@greendeck.co>
2020-01-02 17:10:53 +0530

    initial

--- datasets/base.py
+++ datasets/base.py
@@ -4,6 +4,7 @@ import random
 
 DEBUG_SMALL_DATASET = False
 
+
 class BaseDataset(object):
     def __init__(self, params):
         self._params = params
@@ -15,16 +16,22 @@ class BaseDataset(object):
 
     def params(self):
         return self._params
+
     def load(self):
         raise NotImplementedError()
+
     def x_train(self):
         raise NotImplementedError()
+
     def x_test(self):
         raise NotImplementedError()
+
     def y_train(self):
         raise NotImplementedError()
+
     def y_test(self):
         raise NotImplementedError()
+
     def get_tb_image_varibles(self, x_input, y_input, x_output, y_output):
         raise NotImplementedError()
 
@@ -33,7 +40,7 @@ class MNISTDataset(BaseDataset):
     def load(self):
         with tf.Session() as sess:
             mnist = tf.keras.datasets.mnist
-            (z_train, label_train),(z_test, label_test) = mnist.load_data()
+            (z_train, label_train), (z_test, label_test) = mnist.load_data()
             if DEBUG_SMALL_DATASET and platform.system() == "Darwin":
                 random.shuffle(z_train)
                 z_train = z_train[:1000]
@@ -42,12 +49,14 @@ class MNISTDataset(BaseDataset):
             half_sz = img_sz // 2
             x_train, y_train = tf.split(z_train, 2, axis=2)
             x_test, y_test = tf.split(z_test, 2, axis=2)
+
             def flatten(z):
                 return tf.reshape(tf.cast(z, dtype=tf.float32) / 255.0, shape=(z.shape[0], half_sz))
+
             # left half of image
             self._x_train = sess.run(flatten(x_train))
             self._x_test = sess.run(flatten(x_test))
-            random.shuffle(self._x_test) # for some reason model.evaluate does not have suffle=True parameter
+            random.shuffle(self._x_test)  # for some reason model.evaluate does not have suffle=True parameter
             # right half of image
             self._y_train = sess.run(flatten(y_train))
             self._y_test = sess.run(flatten(y_test))
@@ -55,25 +64,29 @@ class MNISTDataset(BaseDataset):
 
     def x_train(self):
         return self._x_train
+
     def x_test(self):
         return self._x_test
+
     def y_train(self):
         return self._y_train
+
     def y_test(self):
         return self._y_test
+
     def get_tb_image_varibles(self, x_input, y_input, x_output, y_output):
         """Image for tensor board monitoring"""
         shape1 = (28, 14)
-        # Create an image of 4 halves:
-        # x_input=original left half, y_output=predicted right half, x_output=predicted left-half, y_input=original left half
+        # Create an image of 4 halves: x_input=original left half, y_output=predicted right half, x_output=predicted
+        # left-half, y_input=original left half
         img_tensors = [tf.reshape(tf.cast(t[0] * 255, tf.uint8), shape=shape1) for t in
-                       [x_input ,y_output, x_output, y_input]]
+                       [x_input, y_output, x_output, y_input]]
         full_img = tf.concat(img_tensors, axis=1)
 
         img_var = tf.keras.backend.variable(tf.zeros(shape=(shape1[0], shape1[1] * 4), dtype=tf.uint8),
-                                        name="x_y_in_out", dtype=tf.uint8)
+                                            name="x_y_in_out", dtype=tf.uint8)
 
         def dummy_metic_for_images(_y_true_unused, _y_pred_unused):
             return tf.reduce_sum(tf.assign(img_var, full_img))
-        return dummy_metic_for_images, [img_var]
 
+        return dummy_metic_for_images, [img_var]
--- run_model.py
+++ run_model.py
@@ -8,7 +8,7 @@
 # from testing import test_model
 #
 # to run with dirsync, ngrok on Google colab:
-#$ python ~/miscutil/dirsync.py --source  --dir . --rex_include "." --server http://9d963e8b.ngrok.io
+# $ python ~/miscutil/dirsync.py --source  --dir . --rex_include "." --server http://9d963e8b.ngrok.io
 
 import configparser
 import math
@@ -27,6 +27,7 @@ import tensorboardimage
 from tensorflow.python.keras import backend as K
 from tensorflow.keras.callbacks import TensorBoard
 
+
 def create_dataset(name, config):
     import params
     cls_params = params.get_params_class_for_dataset_name(name)
@@ -34,21 +35,24 @@ def create_dataset(name, config):
     data_loader = params.DATA_CLASS(params)
     return data_loader
 
+
 # TODO(franji): remove if not used for gamma regulazation in BatchNormalization
 def inverse_l2_reg_func(coeff):
     def reg(weight_matrix):
-        return K.sum(coeff * K.square(1.0/weight_matrix))
+        return K.sum(coeff * K.square(1.0 / weight_matrix))
+
     return reg
+
 
 BOOKMARK_REPRESENTATION_LAYER = "representation_layer"
 
 
 class LearningRateControl(object):
-    def __init__(self, min_lr,max_lr, step_max_lr, step_min_lr, tensorboardimage=None):
+    def __init__(self, min_lr, max_lr, step_max_lr, step_min_lr, tensorboardimage=None):
         self.tensorboardimage = tensorboardimage
         self.added_scalar_to_tensorboard = False
-        assert(step_min_lr > step_max_lr)  # starting accelerate, decay towards the end
-        assert(max_lr > min_lr)
+        assert (step_min_lr > step_max_lr)  # starting accelerate, decay towards the end
+        assert (max_lr > min_lr)
         self.min_lr = min_lr
         self.max_lr = max_lr
         self.step_max_lr = step_max_lr
@@ -64,10 +68,10 @@ class LearningRateControl(object):
             # if not set - create a variable
             self.global_step = K.variable(tf.zeros(shape=(), dtype=tf.int64), dtype=tf.int64, name="lr_global_step")
             tf.train.global_step(K.get_session(), self.global_step)
-            gs = K.update_add(self.global_step, 1) ###tf.train.get_global_step()
+            gs = K.update_add(self.global_step, 1)  ###tf.train.get_global_step()
         else:
             self.global_step = gs
-        assert(gs is not None)
+        assert (gs is not None)
         gstep = tf.cast(gs, dtype=tf.float32)
         lr_up = K.exp(self.step_accelerate_log * gstep) * self.min_lr
         lr_down = K.exp(self.step_deccelerate_log * (gstep - self.step_max_lr)) * self.max_lr
@@ -77,42 +81,45 @@ class LearningRateControl(object):
             self.added_scalar_to_tensorboard = True  # add once
         return lr
 
+
 def scale1(x):
     x_min = tf.reduce_min(x)
     x_max = tf.reduce_max(x)
     return (x - x_min) / (x_max - x_min)
 
+
 def get_cov_image_varibles(cov_x, cov_y):
     """Image for tensor board monitoring - covariance matrices"""
     # Create an image of 2 halves: cov_x, cov_y
-    img_tensors = [tf.cast(scale1(t) * 255.0 , tf.uint8) for t in [cov_x, cov_y]]
+    img_tensors = [tf.cast(scale1(t) * 255.0, tf.uint8) for t in [cov_x, cov_y]]
     full_img = tf.concat(img_tensors, axis=1)
 
     img_var = tf.keras.backend.variable(tf.zeros_like(full_img), name="cov_x_y", dtype=tf.uint8)
 
     def dummy_metic_for_images(_y_true_unused, _y_pred_unused):
         return tf.reduce_sum(tf.assign(img_var, full_img))
+
     return dummy_metic_for_images, [img_var]
 
 
 def build_model(data_set, tensorboard_callback):
     x_train = data_set.x_train()
     y_train = data_set.y_train()
-    assert(len(x_train.shape) == 2)
-    assert(len(y_train.shape) == 2)
+    assert (len(x_train.shape) == 2)
+    assert (len(y_train.shape) == 2)
     x_input_size = util.shape_i(x_train, 1)
     y_input_size = util.shape_i(y_train, 1)
     x_input = tf.keras.Input(shape=(x_input_size,))
     y_input = tf.keras.Input(shape=(y_input_size,))
     prev_layer_size = x_input_size
-    assert(len(y_train.shape) == 2)
+    assert (len(y_train.shape) == 2)
     y_ouput_size = util.shape_i(y_train, 1)
     layers_x_to_y = []
     layers_y_to_x = []
     is_last_layer = False
     representation_layer_size = None
     for spec in data_set.params().LAYERS_SPEC:
-        assert(not is_last_layer)
+        assert (not is_last_layer)
         is_representation_layer = False
         if len(spec) > 2:
             is_representation_layer = spec[2]
@@ -120,10 +127,10 @@ def build_model(data_set, tensorboard_callback):
         layer_size = spec[1]
         if layer_size == -1:
             layer_size = y_ouput_size
-            is_last_layer = True # size -1 is only for last layer
+            is_last_layer = True  # size -1 is only for last layer
         is_tied = layer_type in [layers.tied.TiedDenseLayer, layers.tied.LocallyDenseLayer]
         layer_kwargs = dict(units=layer_size,
-                         kernel_regularizer=regularizers.l2(data_set.params().WEIGHT_DECAY))
+                            kernel_regularizer=regularizers.l2(data_set.params().WEIGHT_DECAY))
         LXY = layer_type(**layer_kwargs)
         activation_xy = activation_yx = None
         if not is_last_layer:
@@ -142,15 +149,15 @@ def build_model(data_set, tensorboard_callback):
         layer_kwargs = dict(units=prev_layer_size,
                             kernel_regularizer=regularizers.l2(data_set.params().WEIGHT_DECAY))
         if is_tied:
-            layer_kwargs["tied_layer"]=LXY
+            layer_kwargs["tied_layer"] = LXY
         LYX = layer_type(**layer_kwargs)
         print(f"Layer Y->X build {type(LYX)}(kwargs={layer_kwargs})")
         noise_XY = noise_YX = None
         if not is_last_layer and data_set.params().NOISE_LAYER:
             noise_kwargs = dict(rate=data_set.params().DROP_PROBABILITY)
             noise_XY = data_set.params().NOISE_LAYER(**noise_kwargs)
-            print(f"Noise X->Y build {type(noise_XY)}(input_shape={(None,layer_size)})")
-            noise_XY.build(input_shape=(None,layer_size))
+            print(f"Noise X->Y build {type(noise_XY)}(input_shape={(None, layer_size)})")
+            noise_XY.build(input_shape=(None, layer_size))
             if data_set.params().NOISE_LAYER == layers.tied.TiedDropoutLayer:
                 noise_kwargs["tied_layer"] = noise_XY
             noise_YX = data_set.params().NOISE_LAYER(**noise_kwargs)
@@ -197,7 +204,7 @@ def build_model(data_set, tensorboard_callback):
         channel_x_to_y = lay(channel_x_to_y)
         if is_representation_layer:
             # in this channel the bookmark is BEFORE the layer
-            assert(representation_layer_xy is None)
+            assert (representation_layer_xy is None)
             representation_layer_xy = channel_x_to_y
             is_representation_layer = False
 
@@ -218,10 +225,11 @@ def build_model(data_set, tensorboard_callback):
     loss_x = data_set.params().LOSS_X * losses.mean_squared_error(x_input, channel_y_to_x)
     loss_y = data_set.params().LOSS_Y * losses.mean_squared_error(y_input, channel_x_to_y)
     loss_representation = 0.0
-    #assert(representation_layer_xy is not None and representation_layer_yx is not None)
+    # assert(representation_layer_xy is not None and representation_layer_yx is not None)
     if data_set.params().L2_LOSS != 0.0 and representation_layer_xy is not None:
         # loss_representation is named 'loss_l2' in original code.
-        loss_representation = data_set.params().L2_LOSS * losses.mean_squared_error(representation_layer_xy, representation_layer_yx)
+        loss_representation = data_set.params().L2_LOSS * losses.mean_squared_error(representation_layer_xy,
+                                                                                    representation_layer_yx)
 
     loss_withen_x = 0.0
     loss_withen_y = 0.0
@@ -234,12 +242,12 @@ def build_model(data_set, tensorboard_callback):
                       representation_layer_xy) / data_set.params().BATCH_SIZE
         # TODO(Franji): using BACH_SIZE here means in test mode loss_withen_x is wrong
         loss_withen_x = data_set.params().WITHEN_REG_X * (
-                    K.sqrt(K.sum(K.sum(cov_x ** 2))) - K.sqrt(K.sum(tf.diag(cov_x) ** 2)))
+                K.sqrt(K.sum(K.sum(cov_x ** 2))) - K.sqrt(K.sum(tf.diag(cov_x) ** 2)))
     if representation_layer_yx is not None:
         cov_y = K.dot(tf.transpose(representation_layer_yx),
                       representation_layer_yx) / data_set.params().BATCH_SIZE
         loss_withen_y = data_set.params().WITHEN_REG_Y * (
-                    K.sqrt(K.sum(K.sum(cov_y ** 2))) - K.sqrt(K.sum(tf.diag(cov_y) ** 2)))
+                K.sqrt(K.sum(K.sum(cov_y ** 2))) - K.sqrt(K.sum(tf.diag(cov_y) ** 2)))
 
     def combined_loss(_y_true_unused, _y_pred_unused):
         return loss_x + loss_y + loss_representation + loss_withen_x + loss_withen_y
@@ -267,17 +275,20 @@ def build_model(data_set, tensorboard_callback):
         tensorboardimage=tensorboard_callback)
     optimizer = tf.train.MomentumOptimizer(
         use_nesterov=True,
-        learning_rate=learning_rate_control, ###data_set.params().BASE_LEARNING_RATE,
+        learning_rate=learning_rate_control,  ###data_set.params().BASE_LEARNING_RATE,
         momentum=data_set.params().MOMENTUM)
-    #if tensorboard_callback:
+
+    # if tensorboard_callback:
     #    tensorboard_callback.add_scalar("combined_loss", combined_loss(0,0))
     def metric_learning_rate(_y_true_unused, _y_pred_unused):
         return learning_rate_control()
+
     def calculate_cca():
-        return util.cross_correlation_analysis(representation_layer_xy, representation_layer_yx, representation_layer_size)
+        return util.cross_correlation_analysis(representation_layer_xy, representation_layer_yx,
+                                               representation_layer_size)
 
     def metric_cca(_y_true_unused, _y_pred_unused):
-        #return K.switch(K.learning_phase(), tf.constant(0.0), calculate_cca)
+        # return K.switch(K.learning_phase(), tf.constant(0.0), calculate_cca)
         return calculate_cca()
 
     def metric_var_x(_y_true_unused, _y_pred_unused):
@@ -286,16 +297,15 @@ def build_model(data_set, tensorboard_callback):
     def metric_var_y(_y_true_unused, _y_pred_unused):
         return K.mean(K.var(representation_layer_yx))
 
-
     model.compile(optimizer, loss=combined_loss,
                   metrics=[
-                            #dummy_metic_for_images,
-                            #metric_learning_rate,
-                            #dummy_metric_for_cov,
-                            metric_cca,
-                            metric_var_x,
-                            metric_var_y,
-                           ])
+                      # dummy_metic_for_images,
+                      # metric_learning_rate,
+                      # dummy_metric_for_cov,
+                      metric_cca,
+                      metric_var_x,
+                      metric_var_y,
+                  ])
     return model
 
 
@@ -310,29 +320,34 @@ def load_data_set(data_set_config):
     data_set.load()
     return data_set
 
+
 def train_model(data_set, tensorboard_callback):
     model = build_model(data_set, tensorboard_callback)
+    print("=================================================")
+    print(model.summary())
+    print("=================================================")
     model.fit([data_set.x_train(), data_set.y_train()],
               [data_set.y_train(), data_set.x_train()],
               epochs=data_set.params().EPOCH_NUMBER,
-              #steps_per_epoch=data_set.x_train().shape.dims[0].value // data_set.params().BATCH_SIZE,
+              # steps_per_epoch=data_set.x_train().shape.dims[0].value // data_set.params().BATCH_SIZE,
               batch_size=data_set.params().BATCH_SIZE,
               callbacks=[tensorboard_callback],
               shuffle=True,
               )
     return model
 
+
 def test_model(model, data_set, tensorboard_callback, test_batch_size):
     x_test = data_set.x_test()
     y_test = data_set.y_test()
-    assert(len(x_test.shape) == 2)
-    assert(len(y_test.shape) == 2)
+    assert (len(x_test.shape) == 2)
+    assert (len(y_test.shape) == 2)
     res = model.evaluate([x_test, y_test], [y_test, x_test],
                          batch_size=test_batch_size,
-                         #callbacks=[tensorboard_callback],
+                         # callbacks=[tensorboard_callback],
                          )
 
-    print(list(zip(model.metrics_names,res)))
+    print(list(zip(model.metrics_names, res)))
 
 
 def check_data(data_set, tensorboard_callback):
@@ -340,11 +355,12 @@ def check_data(data_set, tensorboard_callback):
     y_test = data_set.y_test()
     print(util._eval_tensor(util.cross_correlation_analysis(x_test, y_test, 50, False)))
 
+
 def train_and_test(dataset_file_ini):
     data_set = load_data_set(dataset_file_ini)
     tensorboard_callback = tensorboardimage.create_tensorboard_callback()
     m = train_model(data_set, tensorboard_callback)
-    #m.save("model2way.h5")
+    # m.save("model2way.h5")
     test_model(m, data_set, tensorboard_callback, data_set.params().BATCH_SIZE)
     test_model(m, data_set, tensorboard_callback, 6000)
     ##check_data(data_set, tensorboard_callback)
@@ -356,6 +372,7 @@ def main(argv):
         return 3
     train_and_test(argv[1])
     return 0
+
 
 if __name__ == '__main__':
     main(sys.argv)
